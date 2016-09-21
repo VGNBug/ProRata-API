f57844003bf564083cc21c18f99fb516ce8585fe
Daniel Pawsey <daniel.pawsey@live.co.uk>
2016-09-21 15:50:27 +0100

    Added supressive readAll() method to ProrataUserController.

--- src/main/java/com/pawsey/prorata/api/controller/ProrataUserController.java
+++ src/main/java/com/pawsey/prorata/api/controller/ProrataUserController.java
@@ -3,10 +3,12 @@ package com.pawsey.prorata.api.controller;
 import com.pawsey.api.rest.controller.BaseRestController;
 import com.pawsey.prorata.api.service.ProrataUserService;
 import com.pawsey.prorata.model.ProrataUserEntity;
+import org.omg.CORBA.Request;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import javax.persistence.EntityNotFoundException;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/user")
@@ -36,9 +38,14 @@ public class ProrataUserController extends BaseRestController<ProrataUserEntity,
     //@formatter:off
     public ProrataUserEntity read( @PathVariable String email,
                                    @PathVariable String password
-    )throws EntityNotFoundException
-    //@formatter:on
-    {
+                                   //@formatter:on
+    )throws EntityNotFoundException {
         return service.signIn(email, password);
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    @ResponseStatus(HttpStatus.FORBIDDEN)
+    public List<ProrataUserEntity> readAll() {
+        return null;
     }
 }
