4e27f6648c780bb5f864622c0e119ecc663727ee
Daniel Pawsey <daniel.pawsey@live.co.uk>
2016-10-02 17:44:26 +0100

    ProrataUserController WIP.

--- src/main/java/com/pawsey/prorata/api/service/impl/ProrataUserServiceImpl.java
+++ src/main/java/com/pawsey/prorata/api/service/impl/ProrataUserServiceImpl.java
@@ -4,155 +4,67 @@ import com.pawsey.api.service.impl.BaseServiceImpl;
 import com.pawsey.prorata.api.repository.*;
 import com.pawsey.prorata.api.service.ProrataUserService;
 import com.pawsey.prorata.model.*;
-import org.aspectj.weaver.Iterators;
 import org.hibernate.Hibernate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataRetrievalFailureException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.persistence.EntityNotFoundException;
 import javax.security.auth.login.CredentialException;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.Collection;
 import java.util.List;
 
 @Service
 public class ProrataUserServiceImpl extends BaseServiceImpl<ProrataUserEntity, ProrataUserRepository> implements ProrataUserService {
 
     @Autowired
-    private BankRepository bankRepository;
-    @Autowired
-    private AccountRepository accountRepository;
-    @Autowired
-    private EmployerRepository employerRepository;
-    @Autowired
-    private SubscriptionRepository subscriptionRepository;
-    @Autowired
-    private EmploymentRepository employmentRepository;
-    @Autowired
-    private UserContactRepository userContactRepository;
-    @Autowired
-    private SubscriptionTypeRepository subscriptionTypeRepository;
+    private SubscriptionTypeRepository subscriptionTypeJpaRepository;
 
-//    @Override
-//    @Transactional
-//    public ProrataUserEntity create(ProrataUserEntity newUser) {
-//        return super.create(newUser);
-//    }
+    @Autowired
+    private SubscriptionRepository subscriptionJpaRepository;
+
+    @Autowired
+    private AccountRepository accountJpaRepository;
+
+    @Autowired
+    private EmploymentRepository employmentJpaRepository;
+
+    @Autowired
+    private EmployerRepository employerJpaRepository;
+
+    @Autowired
+    private UserContactRepository userContactJpaRepository;
+
+    @Autowired
+    private BankRepository bankJpaRepository;
 
     @Override
     @Transactional
     public ProrataUserEntity create(ProrataUserEntity user) {
-        String stateMessage = null;
+        ProrataUserEntity persistedUser = super.create(user);
+        persistCollections(user, persistedUser);
 
-        if (user != null) {
-            if (user.getEmail() != null && user.getPassword() != null) {
-                // Create the user.
-                ProrataUserEntity response = repository.save(user);
-
-                if (response != null) {
-                    stateMessage = "User with email " + user.getEmail() + " created successfully. State is as follows: "
-                            + user.toString();
-                    LOGGER.info(stateMessage);
-
-                    // Persist all collections included with this user.
-                    persistCollections(user, response);
-
-                    // Set the user with a default subscription if they do not
-                    // have one included.
-                    if (user.getListOfSubscription() == null || user.getListOfSubscription().isEmpty()) {
-                        List<SubscriptionEntity> subs = new ArrayList<>();
-                        subs.add(setSubscription(response, 0));
-                        user.setListOfSubscription(subs);
-//                        this.setSubscription(response, 0);
-                    }
-
-                    return this.initializeCollections(response);
-                } else {
-                    stateMessage = "User with email " + user.getEmail() + " was not updated successfully.";
-                    IllegalArgumentException e = new IllegalArgumentException(stateMessage);
-                    LOGGER.error(e);
-                    throw e;
-                }
-            } else if (user.getEmail() == null && user.getPassword() != null) {
-                stateMessage = "update user failed: Email cannot be null.";
-                IllegalArgumentException e = new IllegalArgumentException(stateMessage);
-                LOGGER.error(e);
-                throw e;
-            } else if (user.getEmail() != null && user.getPassword() == null) {
-                stateMessage = "update user failed: Password cannot be null.";
-                IllegalArgumentException e = new IllegalArgumentException(stateMessage);
-                LOGGER.error(e);
-                throw e;
-            } else {
-                stateMessage = "update user failed: Neither email nor password can be null";
-                IllegalArgumentException e = new IllegalArgumentException(stateMessage);
-                LOGGER.error(e);
-                throw e;
-            }
-        } else {
-            stateMessage = "No user was supplied to be updated.";
-            IllegalArgumentException e = new IllegalArgumentException(stateMessage);
-            LOGGER.error(e);
-            throw e;
+        if(user.getListOfSubscription() == null || user.getListOfSubscription().isEmpty()) {
+            setDefaultSubscription(persistedUser);
         }
-    }
 
-    private SubscriptionEntity setSubscription(ProrataUserEntity user, Integer subscriptionTypeId) {
-        String stateMessage = null;
-        SubscriptionEntity subscription = new SubscriptionEntity();
-
-        if (user.getProrataUserId() != null && subscriptionTypeRepository.findOne(subscriptionTypeId) != null) {
-            SubscriptionTypeEntity subType = subscriptionTypeRepository.findOne(subscriptionTypeId);
-        } else if (subscriptionTypeRepository.findOne(subscriptionTypeId) == null) {
-            SubscriptionTypeEntity standard = new SubscriptionTypeEntity();
-            standard.setName("standard");
-            standard.setRate(new BigDecimal(0));
-            subscription.setSubscriptionType(subscriptionTypeRepository.save(standard));
-        } else if (user.getProrataUserId() == null) {
-            stateMessage = "Adding subscription to user failed: User must be persisted and must have an ID in order to add a subscription to them";
-            IllegalArgumentException e = new IllegalArgumentException(stateMessage);
-            LOGGER.error(e);
-            throw e;
-        } else {
-            stateMessage = "Adding subscription to user failed due to an unknown error.";
-            IllegalArgumentException e = new IllegalArgumentException(stateMessage);
-            LOGGER.error(e);
-            throw e;
-        }
-        subscription.setProrataUser(user);
-        subscription.setSubscriptionType(subscriptionTypeRepository.findOne(subscriptionTypeId));
-        subscription.setStartDateTime(Calendar.getInstance().getTime());
-        return this.subscriptionRepository.save(subscription);
-    }
-
-    @Override
-    @Transactional
-    public ProrataUserEntity signIn(String emailHash, String passwordHash) throws CredentialException {
-        try {
-            return checkCredentialsForProrataUserEntity(emailHash, passwordHash);
-        } catch (EntityNotFoundException | CredentialException e) {
-            throw e;
-        }
+        return checkCredentials(persistedUser.getEmail(), persistedUser.getPassword());
     }
 
     @Override
     @Transactional
     public ProrataUserEntity update(ProrataUserEntity user, String email, String password) throws CredentialException {
-        String stateMessage = null;
+        String stateMessage;
 
         if (user != null && email != null && password != null) {
-            ProrataUserEntity checkedUser = checkCredentialsForProrataUserEntity(email, password);
+            ProrataUserEntity checkedUser = checkCredentials(email, password);
 
             if (checkedUser != null) {
                 user.setProrataUserId(checkedUser.getProrataUserId());
 
-                // Update the user.
-                ProrataUserEntity persistedUser = repository.save(user);
-
-                // Persist all collections included with this user.
+                ProrataUserEntity persistedUser = super.update(user);
                 persistCollections(user, persistedUser);
 
                 return persistedUser;
@@ -173,34 +85,69 @@ public class ProrataUserServiceImpl extends BaseServiceImpl<ProrataUserEntity, P
 
     @Override
     @Transactional
-    public void delete(String email, String password) throws CredentialException {
-        final ProrataUserEntity entityToBeDeleted = checkCredentialsForProrataUserEntity(email, password);
-        if (entityToBeDeleted != null) {
-            super.delete(entityToBeDeleted);
-        } else {
-            throw new CredentialException("Unable to find user to be deleted using the supplied credentials");
-        }
-    }
+    public ProrataUserEntity signIn(String email, String password) throws CredentialException {
+        String stateMessage = null;
+        ProrataUserEntity response;
+        if (email != null && password != null) {
+            // Get the user
+            response = checkCredentials(email, password);
 
-    private ProrataUserEntity checkCredentialsForProrataUserEntity(String emailHash, String passwordHash) throws EntityNotFoundException, CredentialException {
-        ProrataUserEntity response = null;
-        if (!emailHash.isEmpty() && !passwordHash.isEmpty()) {
-            try {
-                response = initializeCollections(repository.findByEmail(emailHash));
-                LOGGER.info("ProrataUserEntity with password " + response.getEmail() + " found");
-            } catch (Exception e) {
-                throw new EntityNotFoundException("No ProrataUser with email address " + emailHash + " found");
-            }
-            if (passwordHash.equals(response.getPassword())) {
-                return response;
+            // Check that the user returned is not null, and report on it.
+            if (response != null) {
+                stateMessage = "User with the following details was recovered by " + this.getClass().getSimpleName()
+                        + ": " + response.toString();
+                LOGGER.info(stateMessage);
             } else {
-                throw new CredentialException();
+                stateMessage = "Sign in attempt failed because the user supplied incorrect credentials: " + getUserForErrorLogging(email, password);
+                CredentialException e = new CredentialException(stateMessage);
+                LOGGER.error(e);
+                throw e;
             }
+        } else if (email != null && password == null) {
+            stateMessage = "Read user failed: password cannot be null.";
+            IllegalArgumentException e = new IllegalArgumentException(stateMessage);
+            LOGGER.error(e);
+            throw e;
+        } else if (email == null && password != null) {
+            stateMessage = "Read user failed: email cannot be null.";
+            IllegalArgumentException e = new IllegalArgumentException(stateMessage);
+            LOGGER.error(e);
+            throw e;
         } else {
-            throw new IllegalArgumentException("Must provide an email address and password.");
+            stateMessage = "Read user failed: email and password must be provided.";
+            IllegalArgumentException e = new IllegalArgumentException(stateMessage);
+            LOGGER.error(e);
+            throw e;
         }
+        return response;
     }
 
+    @Override
+    @Transactional
+    public void delete(String email, String password) {
+        super.delete(checkCredentials(email, password));
+    }
+
+    private ProrataUserEntity checkCredentials(String email, String password) {
+        ProrataUserEntity response = null;
+
+        if (email != null && password != null) {
+            ProrataUserEntity matchByEmail = this.repository.findByEmail(email);
+
+            if (matchByEmail != null && password.equals(matchByEmail.getPassword())) {
+                response = this.initializeCollections(matchByEmail);
+            } else {
+                String stateMessage = "User with email " + email + " was not found.";
+                DataRetrievalFailureException e = new DataRetrievalFailureException(stateMessage);
+                LOGGER.error(e);
+                throw e;
+            }
+        }
+
+        return response;
+    }
+
+    @Transactional
     private void persistCollections(ProrataUserEntity user, ProrataUserEntity persistedUser) {
         String stateMessage = null;
 
@@ -209,9 +156,9 @@ public class ProrataUserServiceImpl extends BaseServiceImpl<ProrataUserEntity, P
                 // TODO no need to persist the bank- this should be provided as
                 // a list in the client rather than letting users create them.
                 for (AccountEntity account : user.getListOfAccount()) {
-                    account.setBank(bankRepository.findOne(1));
+                    account.setBank(bankJpaRepository.findOne(1)); // TODO lulwut... why is this hard coded?!
                     account.setProrataUser(persistedUser);
-                    accountRepository.save(account);
+                    accountJpaRepository.save(account);
                     LOGGER.info("Added the following account to user with ID " + user.getProrataUserId() + ": "
                             + account.toString());
                 }
@@ -222,40 +169,34 @@ public class ProrataUserServiceImpl extends BaseServiceImpl<ProrataUserEntity, P
 
                     if (employment.getEmployer() != null) {
                         employer = employment.getEmployer();
-
                     } else {
                         employer = new EmployerEntity();
                         employer.setName("Undeclared Employer");
                         employment.setEmployer(employer);
+                        employerJpaRepository.save(employer);
+                        LOGGER.info("Created employer for employent " + employment.toString());
                     }
-                    employerRepository.save(employer);
-                    LOGGER.info("Persisted employer for employment " + employment.toString());
+
                     employment.setProrataUser(persistedUser);
-                    employmentRepository.save(employment);
+                    employmentJpaRepository.save(employment);
                     LOGGER.info("Added the following account to user with ID " + user.getProrataUserId() + ": "
                             + employment.toString());
                 }
             }
             if (user.getListOfSubscription() != null) {
                 for (SubscriptionEntity subscription : user.getListOfSubscription()) {
-                    if (subscription.getSubscriptionType() == null) {
-                        SubscriptionTypeEntity subscriptionType;
-                        if (subscriptionTypeRepository.findByName("standard") == null) {
-                            subscriptionType = new SubscriptionTypeEntity();
-                            subscriptionType.setName("standard");
-                            subscriptionType.setRate(new BigDecimal(0));
-                            subscriptionTypeRepository.save(subscriptionType);
-                        }
+                    if (subscription.getSubscriptionType() != null) {
+                        subscription.setProrataUser(persistedUser);
+                        subscriptionJpaRepository.save(subscription);
+                        LOGGER.info("Added the following subscription to user with ID " + user.getProrataUserId() + ": "
+                                + subscription.toString());
                     }
-                    subscription.setProrataUser(persistedUser);
-                    subscription.setSubscriptionType(subscriptionTypeRepository.findByName("standard"));
-                    subscriptionRepository.save(subscription);
                 }
             }
             if (user.getListOfUserContact() != null) {
                 for (UserContactEntity contact : user.getListOfUserContact()) {
                     contact.setProrataUser(persistedUser);
-                    userContactRepository.save(contact);
+                    userContactJpaRepository.save(contact);
                     LOGGER.info("Added the following contact to user with ID " + user.getProrataUserId() + ": "
                             + contact.toString());
                 }
@@ -273,6 +214,27 @@ public class ProrataUserServiceImpl extends BaseServiceImpl<ProrataUserEntity, P
         }
     }
 
+    @Transactional
+    private void setDefaultSubscription(ProrataUserEntity persistedUser) {
+        SubscriptionEntity subscription = new SubscriptionEntity();
+        subscription.setStartDateTime(Calendar.getInstance().getTime());
+        subscription.setProrataUser(persistedUser);
+        if(subscriptionTypeJpaRepository.findByName("standard") != null) {
+            subscription.setSubscriptionType(subscriptionTypeJpaRepository.findByName("standard"));
+        } else {
+            SubscriptionTypeEntity type = new SubscriptionTypeEntity();
+            type.setRate(new BigDecimal(0));
+            type.setName("standard");
+            type = subscriptionTypeJpaRepository.save(type);
+            subscription.setSubscriptionType(type);
+        }
+        SubscriptionEntity persistedSubscription = subscriptionJpaRepository.save(subscription);
+        List<SubscriptionEntity> subs = new ArrayList<>();
+        subs.add(persistedSubscription);
+        persistedUser.setListOfSubscription(subs);
+        repository.save(persistedUser);
+    }
+
     private ProrataUserEntity initializeCollections(ProrataUserEntity user) {
         Hibernate.initialize(user.getListOfSubscription());
         Hibernate.initialize(user.getListOfAccount());
@@ -288,13 +250,38 @@ public class ProrataUserServiceImpl extends BaseServiceImpl<ProrataUserEntity, P
             }
         }
 
-        if (user.getListOfSubscription() != null) {
-            for (SubscriptionEntity sub : user.getListOfSubscription()) {
-                Hibernate.initialize(sub.getSubscriptionType());
+        if(user.getListOfSubscription() != null) {
+            for(SubscriptionEntity subscription : user.getListOfSubscription()) {
+                Hibernate.initialize(subscription.getSubscriptionType());
             }
         }
 
         return user;
     }
 
+    private String getUserForErrorLogging(String email, String password) {
+        StringBuilder msg = new StringBuilder();
+
+        ProrataUserEntity entity = repository.findByEmail(email);
+
+        if (entity != null) {
+            //@formatter:off
+            msg.append(
+                    "\nA ProrataUser was found with the email supplied by the client: "
+                            + email
+                            + "\nThe ID of the user is " + entity.getProrataUserId()
+            );
+            //@formatter:on
+
+            if (password.equals(entity.getPassword())) {
+                msg.append("\nThe ProrataUser's password matches the password supplied by the client");
+            } else {
+                msg.append("\nThe password supplied by the client does not matches the password associated with the user");
+            }
+        } else {
+            msg.append("\nNo user with the email " + email + " could be found");
+        }
+
+        return msg.toString();
+    }
 }
