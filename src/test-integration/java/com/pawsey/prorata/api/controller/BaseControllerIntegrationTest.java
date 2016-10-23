b0f93145442f1de4ee9fa26ba1858e5fc9a772b9
Daniel Pawsey <daniel.pawsey@live.co.uk>
2016-10-23 14:47:27 +0100

    h2 WIP.

--- src/test-integration/java/com/pawsey/prorata/api/controller/BaseControllerIntegrationTest.java
+++ src/test-integration/java/com/pawsey/prorata/api/controller/BaseControllerIntegrationTest.java
@@ -36,7 +36,7 @@ public abstract class BaseControllerIntegrationTest<T> {
 
     @Value("${spring.datasource.url}")
     private  String JDBC_URL;
-    @Value("${spring.datasource.driverClassName}")
+    @Value("${spring.database.driverClassName}")
     private  String DRIVER;
     @Value("${spring.datasource.username}")
     private  String USER;
